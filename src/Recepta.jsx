9f29b34f585ee4b0e4226def5ed2d8de031eb6d5
ricard-gi <ricard.hernandez.gi@gmail.com>
2024-06-28 14:56:45 +0200

    fix

--- src/Recepta.jsx
+++ src/Recepta.jsx
@@ -3,6 +3,7 @@ import ReceptesController from "./controllers/ReceptesController";
 import { useEffect, useState } from "react";
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
+import { Col, Row } from "react-bootstrap";
 
 
 function Recepta() {
@@ -39,23 +40,30 @@ function Recepta() {
 
     return (
         <>
-            <h1>Recepta: {id}</h1>
+            <h1>Recepta</h1>
+            <hr />
+
+            <Row>
+                <Col xs={12} sm={12} md={6} lg={4} xl={4} >
+                    <Card >
+                        <Card.Img variant="top" src={recepta.foto} />
+                        <Card.Body>
+                            <Card.Title>{recepta.nom}</Card.Title>
+                            <Card.Text>
+                                {recepta.descripcio
+                                    ?.replace(/\n/g, "*n*")
+                                    .split("*n*")
+                                    .map((e, i) => <p key={i}>{e}</p>)
+                                }
+                            </Card.Text>
+                            <Link className="btn btn-outline-primary" to="/">Tornar</Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
 
 
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={recepta.foto} />
-                <Card.Body>
-                    <Card.Title>{recepta.nom}</Card.Title>
-                    <Card.Text>
-                    { recepta.descripcio
-                            ?.replace(/\n/g, "*n*")
-                            .split("*n*")
-                            .map((e, i) => <p key={i}>{e}</p>)
-                    }
-                    </Card.Text>
-                    <Link className="btn btn-outline-primary" to="/">Tornar</Link>
-                </Card.Body>
-            </Card>
+
 
 
         </>
